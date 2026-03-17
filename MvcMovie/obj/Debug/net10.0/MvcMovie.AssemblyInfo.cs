b479c0830cae8a2b5c89084b115ba2883beb10c5
Omar Emad <oemad3366@gmail.com>
2026-03-17 09:41:08 +0000

    Adding Control

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67499d826e0c04172c2862ec18792b77f2ed9ecc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dab59771aac3715e20a415c01f04114ed1bb124c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -2,9 +2,6 @@
 
 #starting the app:
 
-
-
 dotnet dev-certs https --trust
 
 ctrl f5 if you are using vs code so that you can run the program
-

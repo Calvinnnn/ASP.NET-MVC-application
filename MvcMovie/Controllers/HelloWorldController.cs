16766a0b8ddcc57b608121d08ff78d0fe134e654
Omar Emad <oemad3366@gmail.com>
2026-03-19 17:22:23 +0200

    I have added Hello world page view

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -7,10 +7,10 @@ public class HelloWorldController : Controller
 {
     // 
     // GET: /HelloWorld/
-    public string Index()
+    public IActionResult Index()
     {
-        return "This is my default action...";
-    }
+        return View();
+    }   
     // 
     // GET: /HelloWorld/Welcome/ 
     public string Welcome(string name, int ID = 1)
--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c17199ead7e9684d00c0a1bd38f842406874fd96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e74a97b59ca592654f93015ad7a56ba0411a34a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

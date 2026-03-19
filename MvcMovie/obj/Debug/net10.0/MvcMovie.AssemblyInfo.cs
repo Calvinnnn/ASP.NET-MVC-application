f31b3378ab5a10030a682a9ce21bec32b3d34e9f
Omar Emad <oemad3366@gmail.com>
2026-03-19 17:49:19 +0200

    i added a welcome method in hello world controller that takes the data from url and store it in view dic object that will pass the data into the hello world view

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e74a97b59ca592654f93015ad7a56ba0411a34a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16766a0b8ddcc57b608121d08ff78d0fe134e654")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
